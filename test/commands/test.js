6e455ea287e00bee06fb9c196568ef7120c4827c
Kanahora <115061442+Kanahora@users.noreply.github.com>
2022-10-20 22:14:58 +1100

    Delete test directory

--- test/commands/test.js
+++
@@ -1,9 +0,0 @@
-const { Command, SlashCommandBuilder } = require('../../lib')
-module.exports = new Command({
-	data: new SlashCommandBuilder()
-		.setName("name")
-		.setDescription("description"),
-	run: async function(interaction) {
-		interaction.reply({ content: "Working.", ephemeral: true });
-	}
-})
--- test/index.js
+++
@@ -1,7 +0,0 @@
-require('dotenv').config({ override: true });
-const { Client } = require("../lib");
-new Client({
-	intents: ["Guilds"],
-	token: process.env.TOKEN,
-	mongo: process.env.MONGO
-}).init("10");
