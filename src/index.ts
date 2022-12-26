90218f45f07b5bf0964bb902f785eed7b8bca74a
callmeglenn <glenn.marcell.tjandra@gmail.com>
2022-12-26 18:44:07 +1100

    Removed discord.js exports, it's better to install them directly

--- src/index.ts
+++ src/index.ts
@@ -3,18 +3,12 @@ import Command from "./classes/command";
 import Event from "./classes/event";
 import Mongo from "./classes/mongo";
 import { Cache } from "./typings/types"
-import { Collection, GatewayIntentBits, SlashCommandBuilder } from "discord.js";
 export {
     Cache,
     Client,
     Command,
     Event,
     Mongo
-};
-export {
-    Collection,
-    GatewayIntentBits,
-    SlashCommandBuilder
 };
 import { model as createModel, Schema } from "mongoose";
 export {
